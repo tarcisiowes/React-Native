1c4fd0071fadd710b03263a9d6496f0cd3586bdb
Tarcisio Wesley <tarcisiowesley@gmail.com>
2022-01-22 19:38:30 -0300

    add pasta src + app

---
+++ exercicios/src/App.js
@@ -0,0 +1,10 @@
+/* eslint-disable prettier/prettier */
+import React from 'react';
+import { Text } from 'react-native';
+
+function App() {
+  const jsx = <Text> Texto </Text>;
+  return jsx;
+}
+
+export default App;
